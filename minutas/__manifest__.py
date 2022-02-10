32f1d490881803bd536fa277f8dca308dc35ed5f
Josue Alfaro <josue.alfaro@xmarts.com>
2022-02-10 09:31:46 -0400

    [ADD] license inside '__manifest__.py' file for 'minutas' module

--- minutas/__manifest__.py
+++ minutas/__manifest__.py
@@ -14,6 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
+    'license': 'AGPL-3',
     'version': '15.0.1.0.0',
 
     # any module necessary for this one to work correctly
